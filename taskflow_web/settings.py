1c52468ff28d0e4e6e5eeb9a413681b9a8058077
MarasaniSaiMahitha <113290773+MarasaniSaiMahitha@users.noreply.github.com>
2026-08-25 15:52:48 +0530

    Update settings.py

--- taskflow_web/settings.py
+++ taskflow_web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9rx5el6*zall!4ql43g5dt)&er4_z+s3i76qizcu6z6_o*3m=l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
